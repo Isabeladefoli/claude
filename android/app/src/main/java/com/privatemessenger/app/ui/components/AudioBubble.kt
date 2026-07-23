3261982fc4548076066e4ee015561ec11c1d44f4
Isabela Defoli <isabelah708@gmail.com>
2026-07-23 16:20:04 +0000

    Fix missing Box import in AudioBubble

--- android/app/src/main/java/com/privatemessenger/app/ui/components/AudioBubble.kt
+++ android/app/src/main/java/com/privatemessenger/app/ui/components/AudioBubble.kt
@@ -3,6 +3,7 @@ package com.privatemessenger.app.ui.components
 import android.media.MediaPlayer
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
